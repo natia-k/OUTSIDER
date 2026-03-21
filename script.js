004ac14c3e175856bf134c2fde30969845f17933
natia-k <natia.kurtanidze55@gmail.com>
2026-03-21 12:30:11 +0100

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,14 @@ setTimeout(() => {
 setTimeout(() => {
   document.getElementById("text2").classList.add("show");
 }, 6000);
+setTimeout(() => {
+  document.getElementById("text1").classList.add("show");
+}, 3000);
+
+setTimeout(() => {
+  document.getElementById("text2").classList.add("show");
+}, 6000);
+
+setTimeout(() => {
+  document.body.classList.add("enter");
+}, 9000);
